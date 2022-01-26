1ffa32327a7163629188013458c4e04d4b3e55a5
FranciscoFavelaNajera <98063834+FranciscoFavelaNajera@users.noreply.github.com>
2022-01-26 15:46:09 -0600

    Add files via upload

---
+++ DemoShapes/src/mainAPP.java
@@ -0,0 +1,51 @@
+import Shapes.Circle;
+import Shapes.Rectangle;
+import Shapes.Shape;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class mainAPP {
+    public static void main(String[] args) {
+        List<Shape> shapes = new ArrayList<>();
+        List<Shape> shapesCopy = new ArrayList<>();
+
+        Circle circle = new Circle();
+        circle.x = 10;
+        circle.y = 20;
+        circle.radius = 15;
+        circle.color = "red";
+        shapes.add(circle);
+
+        Circle anotherCircle = (Circle) circle.clone();
+        shapes.add(anotherCircle);
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.width = 10;
+        rectangle.height = 20;
+        rectangle.color = "blue";
+        shapes.add(rectangle);
+
+        cloneAndCompare(shapes, shapesCopy);
+    }
+
+    private static void cloneAndCompare(List<Shape> shapes, List<Shape> shapesCopy) {
+        for (Shape shape : shapes) {
+            shapesCopy.add(shape.clone());
+        }
+
+        for (int i = 0; i < shapes.size(); i++) {
+            if (shapes.get(i) != shapesCopy.get(i)) {
+                System.out.println(i + ": Las formas son diferentes objetos");
+                if (shapes.get(i).equals(shapesCopy.get(i))) {
+                    System.out.println(i + ": y son identicos");
+                } else {
+                    System.out.println(i + ": pero no son identicos");
+                }
+            } else {
+                System.out.println(i + ": los objetos de las formas son identicos");
+            }
+        }
+    }
+}
+
